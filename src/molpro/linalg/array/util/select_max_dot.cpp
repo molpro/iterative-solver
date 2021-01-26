f9d62cb4b0b1623d26cff3c1f1ad167c847a093a
Marat Sibaev <sibaev.marat@gmail.com>
2021-01-27 08:16:00 +1300

    extend explicit instantiation of select_max_dot

--- src/molpro/linalg/array/util/select_max_dot.cpp
+++ src/molpro/linalg/array/util/select_max_dot.cpp
@@ -6,8 +6,25 @@ template std::map<size_t, double>
 select_max_dot<Span<double>, Span<double>, double, double>(size_t n, const Span<double>& x, const Span<double>& y);
 
 template std::map<size_t, double>
+select_max_dot<std::vector<double>, std::vector<double>, double, double>(size_t n, const std::vector<double>& x,
+                                                                         const std::vector<double>& y);
+
+template std::map<size_t, double>
+select_max_dot<Span<double>, std::vector<double>, double, double>(size_t n, const Span<double>& x,
+                                                                  const std::vector<double>& y);
+
+template std::map<size_t, double>
+select_max_dot<std::vector<double>, Span<double>, double, double>(size_t n, const std::vector<double>& x,
+                                                                  const Span<double>& y);
+
+template std::map<size_t, double>
 select_max_dot_iter_sparse<Span<double>, std::map<size_t, double>, double, double>(size_t n, const Span<double>& x,
                                                                                    const std::map<size_t, double>& y);
+
+template std::map<size_t, double>
+select_max_dot_iter_sparse<std::vector<double>, std::map<size_t, double>, double, double>(
+    size_t n, const std::vector<double>& x, const std::map<size_t, double>& y);
+
 template std::map<size_t, double>
 select_max_dot_sparse<std::map<size_t, double>, std::map<size_t, double>, double, double>(
     size_t n, const std::map<size_t, double>& x, const std::map<size_t, double>& y);
--- src/molpro/linalg/array/util/select_max_dot.h
+++ src/molpro/linalg/array/util/select_max_dot.h
@@ -135,8 +135,24 @@ extern template std::map<size_t, double>
 select_max_dot<Span<double>, Span<double>, double, double>(size_t n, const Span<double>& x, const Span<double>& y);
 
 extern template std::map<size_t, double>
+select_max_dot<std::vector<double>, std::vector<double>, double, double>(size_t n, const std::vector<double>& x,
+                                                                         const std::vector<double>& y);
+
+extern template std::map<size_t, double>
+select_max_dot<Span<double>, std::vector<double>, double, double>(size_t n, const Span<double>& x,
+                                                                  const std::vector<double>& y);
+
+extern template std::map<size_t, double>
+select_max_dot<std::vector<double>, Span<double>, double, double>(size_t n, const std::vector<double>& x,
+                                                                  const Span<double>& y);
+
+extern template std::map<size_t, double>
 select_max_dot_iter_sparse<Span<double>, std::map<size_t, double>, double, double>(size_t n, const Span<double>& x,
                                                                                    const std::map<size_t, double>& y);
+
+extern template std::map<size_t, double>
+select_max_dot_iter_sparse<std::vector<double>, std::map<size_t, double>, double, double>(
+    size_t n, const std::vector<double>& x, const std::map<size_t, double>& y);
 
 extern template std::map<size_t, double>
 select_max_dot_sparse<std::map<size_t, double>, std::map<size_t, double>, double, double>(
