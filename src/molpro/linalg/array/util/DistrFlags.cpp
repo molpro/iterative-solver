3bf3739380d5d52e5c40a0ca261c5c665bda0a76
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2024-06-20 08:08:33 +0100

    Wait in DistrFlags RMA

--- src/molpro/linalg/array/util/DistrFlags.cpp
+++ src/molpro/linalg/array/util/DistrFlags.cpp
@@ -83,17 +83,19 @@ DistrFlags::Proxy::~Proxy() {
 int DistrFlags::Proxy::get() const {
   int val;
   MPI_Get(&val, 1, MPI_INT, m_rank, 0, 1, MPI_INT, m_win);
+  MPI_Win_flush(m_rank, m_win);
   return val;
 }
 
 int DistrFlags::Proxy::replace(int val) {
   int res;
-  if (false) {
+  if (true) {
     MPI_Fetch_and_op(&val, &res, MPI_INT, m_rank, 0, MPI_REPLACE, m_win); // this seems not always to work
   } else {
     MPI_Get(&res, 1, MPI_INT, m_rank, 0, 1, MPI_INT, m_win);
     MPI_Put(&val, 1, MPI_INT, m_rank, 0, 1, MPI_INT, m_win);
   }
+  MPI_Win_flush(m_rank, m_win);
   return res;
 }
 
