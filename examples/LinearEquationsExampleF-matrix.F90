ec68b5de27bf3984266e0ba1748b1300abf76774
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2024-10-01 17:23:15 +0100

    Simplify LinearEquationsExampleF-matrix.F90

--- examples/LinearEquationsExampleF-matrix.F90
+++ examples/LinearEquationsExampleF-matrix.F90
@@ -8,12 +8,9 @@ PROGRAM Linear_Equations_Example
   IMPLICIT NONE
   INTEGER, PARAMETER :: n = 300, nroot = 2
   DOUBLE PRECISION, PARAMETER :: alpha = 300
-  DOUBLE PRECISION, DIMENSION(1), PARAMETER :: augmented_hessian_factors = [0.0_8]! issue 510 , .001_8, .01_8, .1_8, 1.0_8]
   DOUBLE PRECISION, DIMENSION (n, n), target :: m
   DOUBLE PRECISION, DIMENSION (n, nroot), target :: rhs
   DOUBLE PRECISION, DIMENSION (n, nroot) :: c, g
-  DOUBLE PRECISION :: augmented_hessian
-  INTEGER :: iaug
   LOGICAL :: converged
   TYPE(Matrix_Problem) :: problem
   CALL mpi_init
@@ -21,16 +18,12 @@ PROGRAM Linear_Equations_Example
   IF (mpi_rank_global() .gt. 0) CLOSE(6)
   CALL initialise_matrices
   CALL problem%attach(m, rhs)
-  DO iaug = 1, SIZE(augmented_hessian_factors)
-    augmented_hessian = augmented_hessian_factors(iaug)
-    PRINT *, 'solve linear system with augmented hessian factor ', augmented_hessian
-    converged = Solve_Linear_Equations(c, g, problem, augmented_hessian = augmented_hessian, thresh = 1d-11, verbosity = 2, max_p = 30, hermitian = .true.)
-    PRINT *, 'convergence?', converged, ', residual length: ', norm2(g)
-    !    print *,c
-    !    print *,g
-    CALL Iterative_Solver_Print_Statistics
-    CALL Iterative_Solver_Finalize
-  ENDDO
+  converged = Solve_Linear_Equations(c, g, problem, thresh = 1d-11, verbosity = 2, max_p = 30, hermitian = .true.)
+  PRINT *, 'convergence?', converged, ', residual length: ', norm2(g)
+  !    print *,c
+  !    print *,g
+  CALL Iterative_Solver_Print_Statistics
+  CALL Iterative_Solver_Finalize
   CALL mpi_finalize
 CONTAINS
   SUBROUTINE initialise_matrices
