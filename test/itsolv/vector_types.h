49183a1c331f7c8276b1588c62567f39464d8dd9
Robert Adam <dev@robert-adam.de>
2025-11-18 16:49:45 +0100

    Perform tests with R != Q

--- test/itsolv/vector_types.h
+++ test/itsolv/vector_types.h
@@ -3,9 +3,18 @@
 #include <cstddef>
 #include <map>
 #include <vector>
+#include <deque>
+#include <type_traits>
 using scalar = double;
 using Rvector = std::vector<scalar>;
-using Qvector = std::vector<scalar>;
+// Ensure Rvector != Qvector while ensuring that Qvector still exposes the necessary API subset
+// of std::vector - in particular operator[].
+// std::deque just happens to fulfill this criterion
+using Qvector = std::deque<scalar>;
+static_assert(!std::is_constructible_v<Rvector, Qvector>,
+        "We want to test our code works if Rvector can't directly constructed from Qvector");
+static_assert(!std::is_constructible_v<Qvector, Rvector>,
+        "We want to test our code works if Qvector can't directly constructed from Rvector");
 using Pvector = std::map<size_t, scalar>;
 
 #endif // LINEARALGEBRA_TEST_VECTOR_TYPES_H_
