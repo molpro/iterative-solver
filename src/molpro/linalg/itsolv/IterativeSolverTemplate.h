molpro/iterative-solver

commit b3e7e830e464967dae54566825e57bfbe4d2b3ee

Accommodate the possibility that the user-supplied guess for multiroot linear problems contains linear dependencies.
Fixes #533

src/molpro/linalg/itsolv/IterativeSolverTemplate.h
@@ -543,6 +543,8 @@ protected:
     prof->stop();
     set_value_errors();
     m_errors = m_subspace_solver->errors();
+    for (size_t i = m_errors.size(); i < n_roots(); ++i)
+      m_errors.push_back(1);
     m_working_set = detail::select_working_set(parameters.size(), m_errors, m_convergence_threshold, m_value_errors,
                                                m_convergence_threshold_value);
     for (size_t i = 0; i < m_working_set.size(); ++i) {
@@ -553,7 +555,7 @@ protected:
       } else {
         if (root < i)
           throw std::logic_error("incorrect ordering of roots");
-        if (root > i) {
+        if (root > i && root < parameters.size()) {
           m_handlers->rr().copy(parameters[i], parameters[root]);
           m_handlers->rr().copy(action[i], action[root]);
         }

CMakeLists.txt
@@ -38,6 +38,13 @@ message(STATUS "iterative-solver: LINEARALGEBRA_ARRAY_GA=${LINEARALGEBRA_ARRAY_G
 message(STATUS "iterative-solver: LINEARALGEBRA_ARRAY_HDF5=${LINEARALGEBRA_ARRAY_HDF5}")
 ##############################
 
+find_program(CCACHE_PROGRAM ccache)
+if(CCACHE_PROGRAM)
+    set(CMAKE_C_COMPILER_LAUNCHER "${CCACHE_PROGRAM}")
+    set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_PROGRAM}")
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE "${CCACHE_PROGRAM}")
+endif()
+
 
 add_subdirectory(dependencies)
 

src/molpro/linalg/itsolv/LinearEigensystemDavidson.h
@@ -98,7 +98,7 @@ public:
   virtual std::vector<scalar_type> working_set_eigenvalues() const override {
     auto eval = std::vector<scalar_type>{};
     for (auto i : this->working_set()) {
-      eval.emplace_back(this->m_subspace_solver->eigenvalues().at(i));
+      eval.push_back(i < this->m_subspace_solver->eigenvalues().size() ? this->m_subspace_solver->eigenvalues().at(i) : 0);
     }
     return eval;
   }

src/molpro/linalg/itsolv/helper-implementation.h
@@ -7,6 +7,9 @@
 #include <molpro/lapacke.h>
 #include <molpro/linalg/itsolv/helper.h>
 
+#include "Logger.h"
+#include "subspace/Matrix.h"
+
 namespace molpro::linalg::itsolv {
 
 template <typename value_type>
@@ -668,5 +671,56 @@ void solve_DIIS(std::vector<value_type>& solution, const std::vector<value_type>
   }
 }
 } // namespace molpro::linalg::itsolv
+
+
+namespace molpro::linalg::itsolv::detail {
+
+/*!
+ * @brief Deduces a set of parameters that are redundant due to linear dependencies
+ *
+ * Only the last nR parameters are considered for removal. Linea dependencies are discovered by performing SVD of the
+ * overlap matrix.
+ *
+ * @param overlap overlap matrix of the full subspace
+ * @param oR offset to the start of parameter block
+ * @param nR number of parameters to consider for removal
+ * @param svd_thresh singular value threshold for choosing the null space
+ * @param logger logger
+ * @return indices of the last nR parameters that are considered redundant.
+ */
+template <typename value_type, typename value_type_abs>
+auto redundant_parameters(const subspace::Matrix<value_type>& overlap, const size_t oR, const size_t nR,
+                          const value_type_abs svd_thresh, Logger& logger) {
+  auto prof = molpro::Profiler::single();
+  prof->start("itsolv::svd_system");
+  logger.msg("redundant_parameters()", Logger::Trace);
+  auto redundant_params = std::vector<int>{};
+  auto rspace_indices = std::vector<int>(nR);
+  std::iota(std::begin(rspace_indices), std::end(rspace_indices), 0);
+  auto svd = svd_system(overlap.rows(), overlap.cols(),
+                        array::Span(const_cast<value_type*>(overlap.data().data()), overlap.size()), svd_thresh, true);
+  prof->stop();
+  prof->start("find redundant parameters");
+  for (const auto& singular_system : svd) {
+    if (!rspace_indices.empty()) {
+      auto rspace_contribution = std::vector<value_type_abs>{};
+      for (auto i : rspace_indices)
+        rspace_contribution.push_back(std::abs(singular_system.v.at(oR + i)));
+      auto it_min = std::max_element(std::begin(rspace_contribution), std::end(rspace_contribution));
+      auto imin = std::distance(std::begin(rspace_contribution), it_min);
+      redundant_params.push_back(rspace_indices[imin]);
+      rspace_indices.erase(std::begin(rspace_indices) + imin);
+      std::stringstream ss;
+      ss << std::setprecision(3) << "redundant parameter found, i = " << redundant_params.back()
+         << ", svd.value = " << singular_system.value
+         << ", svd.v[i] = " << singular_system.v[oR + redundant_params.back()];
+      logger.msg(ss.str(), Logger::Info);
+    }
+  }
+  prof->stop();
+  return redundant_params;
+}
+
+}
 
 #endif // LINEARALGEBRA_SRC_MOLPRO_LINALG_ITERATIVESOLVER_HELPER_IMPLEMENTATION_H_

src/molpro/linalg/itsolv/propose_rspace.h
@@ -465,52 +465,6 @@ auto modified_gram_schmidt(const VecRef<R>& rparams, const subspace::Matrix<valu
   return null_params;
 }
 
-/*!
- * @brief Deduces a set of parameters that are redundant due to linear dependencies
- *
- * Only the last nR parameters are considered for removal. Linea dependencies are discovered by performing SVD of the
- * overlap matrix.
- *
- * @param overlap overlap matrix of the full subspace
- * @param oR offset to the start of parameter block
- * @param nR number of parameters to consider for removal
- * @param svd_thresh singular value threshold for choosing the null space
- * @param logger logger
- * @return indices of the last nR parameters that are considered redundant.
- */
-template <typename value_type, typename value_type_abs>
-auto redundant_parameters(const subspace::Matrix<value_type>& overlap, const size_t oR, const size_t nR,
-                          const value_type_abs svd_thresh, Logger& logger) {
-  auto prof = molpro::Profiler::single();
-  prof->start("itsolv::svd_system");
-  logger.msg("redundant_parameters()", Logger::Trace);
-  auto redundant_params = std::vector<int>{};
-  auto rspace_indices = std::vector<int>(nR);
-  std::iota(std::begin(rspace_indices), std::end(rspace_indices), 0);
-  auto svd = svd_system(overlap.rows(), overlap.cols(),
-                        array::Span(const_cast<value_type*>(overlap.data().data()), overlap.size()), svd_thresh, true);
-  prof->stop();
-  prof->start("find redundant parameters");
-  for (const auto& singular_system : svd) {
-    if (!rspace_indices.empty()) {
-      auto rspace_contribution = std::vector<value_type_abs>{};
-      for (auto i : rspace_indices)
-        rspace_contribution.push_back(std::abs(singular_system.v.at(oR + i)));
-      auto it_min = std::max_element(std::begin(rspace_contribution), std::end(rspace_contribution));
-      auto imin = std::distance(std::begin(rspace_contribution), it_min);
-      redundant_params.push_back(rspace_indices[imin]);
-      rspace_indices.erase(std::begin(rspace_indices) + imin);
-      std::stringstream ss;
-      ss << std::setprecision(3) << "redundant parameter found, i = " << redundant_params.back()
-         << ", svd.value = " << singular_system.value
-         << ", svd.v[i] = " << singular_system.v[oR + redundant_params.back()];
-      logger.msg(ss.str(), Logger::Info);
-    }
-  }
-  prof->stop();
-  return redundant_params;
-}
-
 //! Returns new working set based on parameters included in wparams
 template <class R>
 auto get_new_working_set(const std::vector<int>& working_set, const CVecRef<R>& params, const CVecRef<R>& wparams) {

src/molpro/linalg/itsolv/subspace/QSpace.h
@@ -9,6 +9,8 @@
 #include <molpro/linalg/itsolv/subspace/Dimensions.h>
 #include <molpro/linalg/itsolv/subspace/PSpace.h>
 #include <molpro/linalg/itsolv/wrap.h>
+#include <molpro/linalg/itsolv/util.h>
+#include <molpro/linalg/itsolv/helper-implementation.h>
 
 namespace molpro::linalg::itsolv::subspace {
 
@@ -82,14 +84,22 @@ struct QSpace {
                        qspace::QParam<Q>{std::make_unique<Q>(m_handlers->qr().copy(params.at(i))),
                                          std::make_unique<Q>(m_handlers->qr().copy(actions.at(i))), m_unique_id++});
     }
-    const size_t nQnew = params.size();
+    size_t nQnew = params.size();
+    if (nQnew>1) {
+      auto s = Matrix<double>({nQnew,nQnew});
+      s.slice() = qq.at(EqnData::S).slice({0,0},{nQnew,nQnew});
+      auto rp = molpro::linalg::itsolv::detail::redundant_parameters(s, 0, nQnew,
+                                                                     1e-8, *m_logger);
+      nQnew -= rp.size();
+      for (auto& p : rp) {m_params.pop_back();}
+    }
     const auto nXnew = dims.nX + nQnew;
     auto data = old_data;
     for (auto d : {EqnData::H, EqnData::S}) {
       data[d].resize({dims.nX + nQnew, dims.nX + nQnew});
       data[d].slice({dims.oQ + nQnew, dims.oQ + nQnew}, {data[d].rows(), data[d].cols()}) =
           old_data[d].slice({dims.oQ, dims.oQ}, {dims.nX, dims.nX});
-      data[d].slice({dims.oQ, dims.oQ}, {dims.oQ + nQnew, dims.oQ + nQnew}) = qq.at(d);
+      data[d].slice({dims.oQ, dims.oQ}, {dims.oQ + nQnew, dims.oQ + nQnew}) = qq.at(d).slice({0,0},{nQnew,nQnew});
       data[d].slice({dims.oQ, 0}, {dims.oQ + nQnew, dims.oQ}) = qx.at(d).slice({0, 0}, {nQnew, dims.oQ});
       data[d].slice({dims.oQ, dims.oQ + nQnew}, {dims.oQ + nQnew, nXnew}) =
           qx.at(d).slice({0, dims.oQ}, {nQnew, dims.nX});

test/itsolv/test_LinearEigensystem.cpp
@@ -139,6 +139,7 @@ struct LinearEigensystemF : ::testing::Test {
     return std::make_tuple(x, g);
   }
 
+  bool repeated_guess = false;
   auto initial_guess(std::vector<Rvector> &x) {
     const auto n_roots = x.size();
     std::vector<size_t> guess;
@@ -148,7 +149,7 @@ struct LinearEigensystemF : ::testing::Test {
     }
     for (size_t root = 0; root < n_roots; root++) {
       guess.push_back(std::min_element(diagonals.begin(), diagonals.end()) - diagonals.begin()); // initial guess
-      *std::min_element(diagonals.begin(), diagonals.end()) = 1e99;
+      if (! repeated_guess) *std::min_element(diagonals.begin(), diagonals.end()) = 1e99;
       x[root][guess.back()] = 1; // initial guess
     }
   }
@@ -234,7 +235,8 @@ struct LinearEigensystemF : ::testing::Test {
       action(x, g);
       solver->add_vector(x, g);
     }
-    auto n_working_vectors_guess = std::max(nroot, n_working_vectors_max > 0 ? n_working_vectors_max : nroot);
+    auto n_working_vectors_guess = std::min(
+      g.size(), std::max(nroot, n_working_vectors_max > 0 ? n_working_vectors_max : nroot));
     x.resize(n_working_vectors_guess);
     g.resize(n_working_vectors_guess);
     update(g, solver->working_set_eigenvalues());
@@ -429,5 +431,14 @@ TEST_F(LinearEigensystemF, solution) {
         }
       }
     }
+  }
+}
+
+TEST_F(LinearEigensystemF, linearly_dependent_guess) {
+  LinearEigensystemF::repeated_guess = true;
+  LinearEigensystemF::verbosity = 99;
+  for (const size_t n : std::vector<size_t>{2}) {
+      load_matrix(n, "", (double)1);
+      test_eigen(std::to_string(n));
   }
 }

commit a8e5bd7bcc2a1bdf89183fe0d6f69e8cc1f4f981

Don't pad error vector with artificial entries
This padding, introduced in b3e7e830e464967dae54566825e57bfbe4d2b3ee
could lead to the error vector being larger than the temp_solutions
vector. In case the actual entries in the error vector (before padding)
are all smaller than the padding value (1), select_working_set will
select the indices of the padded entries into the working set. This will
subsequently lead to out-of-bounds accesses on temp_solutions.

Since the purpose of this padding is not quite clear in the first place,
this commit gets rid of it entirely.

Fixes #552

src/molpro/linalg/itsolv/IterativeSolverTemplate.h
@@ -542,8 +542,6 @@ protected:
     prof->stop();
     set_value_errors();
     m_errors = m_subspace_solver->errors();
-    for (size_t i = m_errors.size(); i < n_roots(); ++i)
-      m_errors.push_back(1);
     m_working_set = detail::select_working_set(parameters.size(), m_errors, m_convergence_threshold, m_value_errors,
                                                m_convergence_threshold_value);
     for (size_t i = 0; i < m_working_set.size(); ++i) {