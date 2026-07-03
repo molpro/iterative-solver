5b57069e80f259feb2895d41b4ebe601d4299c8b
Robert Adam <dev@robert-adam.de>
2026-07-03 13:39:35 +0200

    Fix assertion
    Since we are dividing by sqrt(ovl), it must be positive and strictly
    greater than zero. Otherwise, the division is undefined behavior.

--- src/molpro/linalg/itsolv/helper-implementation.h
+++ src/molpro/linalg/itsolv/helper-implementation.h
@@ -439,7 +439,7 @@ void eigenproblem(std::vector<value_type>& eigenvectors, std::vector<value_type>
         auto ovl = subspaceEigenvectors.col(k).dot(S * subspaceEigenvectors.col(k));
         // S is supposed to be positive (semi-)definite implying that ovl must be a non-negative real number
         assert(std::abs(ovl.imag()) < 1e-10);
-        assert(ovl.real() >= 0);
+        assert(ovl.real() > 0);
         subspaceEigenvectors.col(k) /= std::sqrt(ovl.real());
       }
   }
